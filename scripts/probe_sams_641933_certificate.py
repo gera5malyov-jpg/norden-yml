db10bc503b550cb8dc6b64202cc26d653a8fa386
gera5malyov-jpg <gera5malyov@gmail.com>
2026-09-24 12:23:35 +0300

    Probe Ozon EAEU declaration parameters

--- scripts/probe_sams_641933_certificate.py
+++ scripts/probe_sams_641933_certificate.py
@@ -48,7 +48,8 @@ for label,path,body in [
     ("OPTIONS_EMPTY","/v2/product/certification/options",{}),
     ("PARAMS_TYPE_ONLY","/v2/product/certification/params",{"params":{
         "certificate_type":"DECLARATION",
-        "certificate_country":"RU"
+        "certificate_country":"RU",
+        "accordance_type":"EAEU"
     }}),
     ("PARAMS_CORE","/v2/product/certification/params",{"params":{
         "name":"Декларация о соответствии",
