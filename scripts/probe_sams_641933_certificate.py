b1f6956e8501eeff8afcc5388a93c31189cfc9af
gera5malyov-jpg <gera5malyov@gmail.com>
2026-09-24 12:22:48 +0300

    Probe Ozon declaration parameters for RU

--- scripts/probe_sams_641933_certificate.py
+++ scripts/probe_sams_641933_certificate.py
@@ -48,7 +48,7 @@ for label,path,body in [
     ("OPTIONS_EMPTY","/v2/product/certification/options",{}),
     ("PARAMS_TYPE_ONLY","/v2/product/certification/params",{"params":{
         "certificate_type":"DECLARATION",
-        "certificate_country":"unknown"
+        "certificate_country":"RU"
     }}),
     ("PARAMS_CORE","/v2/product/certification/params",{"params":{
         "name":"Декларация о соответствии",
